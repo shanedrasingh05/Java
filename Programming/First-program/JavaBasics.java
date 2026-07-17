a4ff738ae7f98167948c2933f3e2c51d20226bf6
Shanedra Singh <shanedrasingh89@gmail.com>
2026-07-17 21:36:32 +0530

    type conversion

--- Programming/First-program/JavaBasics.java
+++ Programming/First-program/JavaBasics.java
@@ -2,8 +2,15 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String args[]){
 
-        long a = 25;
-        int b = a;
-        System.out.println(b);
+        // long a = 25;
+        // int b = a;
+        // System.out.println(b);
+
+        Scanner sc = new Scanner(System.in);
+
+        // Float 
+        float number = sc.nextFloat();
+
+        System.out.println(number);
     }
-}
+
