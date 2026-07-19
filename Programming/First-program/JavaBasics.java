88028c0c3e8c0945efef20f02e86bf426fa84a26
Shanedra Singh <shanedrasingh89@gmail.com>
2026-07-19 22:29:49 +0530

    delete code

---
+++ Programming/First-program/JavaBasics.java
@@ -0,0 +1,38 @@
+// import java.util.*;
+// public class JavaBasics {
+//     public static void main(String args[]){
+
+//         // long a = 25;
+//         // int b = a;
+//         // System.out.println(b);
+
+//         Scanner sc = new Scanner(System.in);
+
+//         // Float 
+//         float number = sc.nextFloat();
+
+//         System.out.println(number);
+//     }
+
+// }
+
+// **********Type casting ***************
+
+// import java.util.*;
+// public class JavaBasics {
+//     public static void main(String args[]){
+
+  
+
+//         Scanner sc = new Scanner(System.in);
+
+//         // Float 
+//            float a = 25.12f;
+//      int b = (int)a;
+//         float number = sc.nextFloat();
+
+//         System.out.println(b);
+//     }
+
+// }
+
