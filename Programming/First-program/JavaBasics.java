5c01783a14c75283954b6398107a92c598c6e7ff
Shanedra Singh <shanedrasingh89@gmail.com>
2026-07-19 22:47:16 +0530

    byte

--- Programming/First-program/JavaBasics.java
+++ Programming/First-program/JavaBasics.java
@@ -35,4 +35,19 @@
 //     }
 
 // }
+// 
+// ******** TYPE OF PROMOTION **********
+import java.util.*;
 
+public class JavaBasics {
+
+    public static void main(String args[]){
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+
+        byte bt = (byte) (a+b+c);
+
+        System.out.println(bt);
+    }
+}
