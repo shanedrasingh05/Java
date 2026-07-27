b39ecb3f9bbfe43825448625faa2e0a3dcb70a49
Shanedra Singh <shanedrasingh89@gmail.com>
2026-07-27 21:51:09 +0530

    operator

---
+++ Programming/Operators/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;;
+
+public class Main {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+
+        // System.out.println(A+B);
+        // System.out.println(A-B);
+        // System.out.println(A*B);
+        // System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
