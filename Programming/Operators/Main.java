3453a9a1985b825e9f2da30734c7e747085e03e3
Shanedra Singh <shanedrasingh89@gmail.com>
2026-08-01 23:14:57 +0530

    Assignment-1

--- Programming/Operators/Main.java
+++ Programming/Operators/Main.java
@@ -1,14 +1,14 @@
-import java.util.*;;
+// import java.util.*;;
 
-public class Main {
-    public static void main(String[] args) {
-        int A = 10;
-        int B = 5;
+// public class Main {
+//     public static void main(String[] args) {
+//         int A = 10;
+//         int B = 5;
 
-        // System.out.println(A+B);
-        // System.out.println(A-B);
-        // System.out.println(A*B);
-        // System.out.println(A/B);
-        System.out.println(A%B);
-    }
-}
+//         // System.out.println(A+B);
+//         // System.out.println(A-B);
+//         // System.out.println(A*B);
+//         // System.out.println(A/B);
+//         System.out.println(A%B);
+//     }
+// }
